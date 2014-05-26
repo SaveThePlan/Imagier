a3b5aad20e3b5d6b97c7cf933904f96bc559d0e7
SaveThePlan <realnanook@gmail.com>
2014-05-26 12:53:10 +0200

    Initial commit from local git

---
+++ Imagier/STPViewController.h
@@ -0,0 +1,13 @@
+//
+//  STPViewController.h
+//  Imagier
+//
+//  Created by Nanook on 26/05/2014.
+//  Copyright (c) 2014 SaveThePlan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPViewController : UIViewController
+
+@end
---
+++ Imagier/STPViewController.m
@@ -0,0 +1,29 @@
+//
+//  STPViewController.m
+//  Imagier
+//
+//  Created by Nanook on 26/05/2014.
+//  Copyright (c) 2014 SaveThePlan. All rights reserved.
+//
+
+#import "STPViewController.h"
+
+@interface STPViewController ()
+
+@end
+
+@implementation STPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
